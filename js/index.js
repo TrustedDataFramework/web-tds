77d5b7212e02c07474145163b36ef179cb659af2
zhaoqi <1223996281@qq.com>
2020-09-27 14:27:40 +0800

    update tds-web

--- js/index.js
+++ js/index.js
@@ -1,17 +1,12 @@
 $(function() {
-	$.get("/rpc/stat",
-		function(result) {
-			if(result.code == "200"){
-				setHtml(result.data, 'tpl1', 'block_data_browser');
-				let height = result.data.height;
-				$.get("/rpc/block/"+height,
-					function(result1) {
-						if(result1.code == "200"){
-							setHtml(result1.data, 'tpl', 'block_data_test');
-						}
-					}
-				);
-			}
-		}
+
+	$.ajax(
+	{
+		url:"/WisdomCore/ExplorerInfo",
+			success:function(result){
+			setHtml(result.data, 'tpl1', 'block_data_browser');},
+			error:function(result){console.log("error");}
+	}
 	);
- });
+});
+
